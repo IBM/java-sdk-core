11d4ff6d23359ebf30ae29f06b2f0887b40ad3f9
Phil Adams <phil_adams@us.ibm.com>
2019-02-13 08:38:04 -0600

    Fixed pre- and post-deployment steps

--- build/sync2MC.sh
+++ build/sync2MC.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-
+#
+# sync2MC.sh - invoke the "maven_central_sync" bintray REST API to request that the specified 
+#              bintray package be synced to maven central.
+#
+# Syntax:
+#    sync2MC.sh <bintray-user> <bintray-apikey> <bintray-reponame> <bintray-packagename> <bintray-packageversion>
+# where:
+#   <bintray-user>           is the bintray username (subject)
+#   <bintray-apikey>         is the api key associated with the bintray user
+#   <bintray-reponame>       is the name of the bintray repository that the package belongs to
+#   <bintray-packagename>    is the name of the bintray package to be synced
+#   <bintray-packageversion> is the version of the package to be synced
+#
+# example:
+#   sync2MC.sh bintrayuser1 <apikey> my-bintray-maven-repo my.group.id:my-package-name 1.1.0
+#
+#
 if [ $# -lt 5 ]
 then
     echo "
