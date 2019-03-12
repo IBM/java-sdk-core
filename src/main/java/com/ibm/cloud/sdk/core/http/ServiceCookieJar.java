2ed1d5c65e1d9214d1b608c298a930d6dedfb248
Logan Patino <loganpatino10@gmail.com>
2019-03-11 23:09:58 -0400

    refactor: Rename cookie jar implementation and remove old Speech to Text logic

--- src/main/java/com/ibm/cloud/sdk/core/http/ServiceCookieJar.java
+++ src/main/java/com/ibm/cloud/sdk/core/http/ServiceCookieJar.java
@@ -12,23 +12,19 @@
  */
 package com.ibm.cloud.sdk.core.http;
 
-import java.net.CookieHandler;
-import java.util.ArrayList;
-import java.util.List;
-
 import okhttp3.Cookie;
 import okhttp3.CookieJar;
 import okhttp3.HttpUrl;
 import okhttp3.JavaNetCookieJar;
 
+import java.net.CookieHandler;
+import java.util.List;
+
 /**
- * This is an adapter that uses {@link JavaNetCookieJar} and ignore Speech to Text sessions for session less requests.
+ * This is an adapter that uses {@link JavaNetCookieJar}.
  *
  */
-public final class WatsonCookieJar implements CookieJar {
-  private static final String SESSIONID = "SESSIONID";
-  private static final String SESSIONS = "sessions";
-  private static final String SPEECH_TO_TEXT = "speech-to-text";
+public final class ServiceCookieJar implements CookieJar {
   private JavaNetCookieJar adapter;
 
   /**
@@ -36,7 +32,7 @@ public final class WatsonCookieJar implements CookieJar {
    *
    * @param cookieHandler the cookie handler
    */
-  public WatsonCookieJar(CookieHandler cookieHandler) {
+  public ServiceCookieJar(CookieHandler cookieHandler) {
     this.adapter = new JavaNetCookieJar(cookieHandler);
   }
 
@@ -58,19 +54,7 @@ public final class WatsonCookieJar implements CookieJar {
    */
   @Override
   public List<Cookie> loadForRequest(HttpUrl url) {
-    List<Cookie> cookies = this.adapter.loadForRequest(url);
-
-    // TODO: Removes the SESSIONID for speech to text session lest requests
-    if (url.encodedPathSegments().contains(SPEECH_TO_TEXT) && !url.encodedPathSegments().contains(SESSIONS)) {
-      List<Cookie> sessionLessCookies = new ArrayList<Cookie>();
-      for (Cookie cookie : cookies) {
-        if (!cookie.name().equalsIgnoreCase(SESSIONID)) {
-          sessionLessCookies.add(cookie);
-        }
-      }
-      cookies = sessionLessCookies;
-    }
-    return cookies;
+    return this.adapter.loadForRequest(url);
   }
 
 }
