e8a2aa864832e930e3aea2329a267e3c5f563b92
Phil Adams <phil_adams@us.ibm.com>
2019-04-16 10:44:50 -0500

    New Dynamic Model pattern to support type-checked additional properties (#23)
    * New Dynamic Model pattern to support type-checked additional properties
    
    Why:
    The SDK code generator is being updated to improve the pattern used for
    dynamic models (i.e. models that support "additionalProperties").
    
    What:
    The changes in this PR can be summarized as follows:
    1) An enhanced DynamicModel class which is now parameterized with the
    additionalProperties value type and includes a map for them
    2) A new DynamicModelTypeAdapterFactory class which is registered with Gson
    to perform serialization and deserialization of ALL generated DynamicModel
    subclasses.
    3) GsonSerializationHelper is now deprecated as it's no longer needed.
    4) Tests related to the above functional changes.
    
    * Addressed review comments

--- src/main/java/com/ibm/cloud/sdk/core/service/model/DynamicModel.java
+++ src/main/java/com/ibm/cloud/sdk/core/service/model/DynamicModel.java
@@ -1,14 +1,103 @@
 package com.ibm.cloud.sdk.core.service.model;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
+import com.google.gson.reflect.TypeToken;
 import com.ibm.cloud.sdk.core.util.GsonSingleton;
 
 /**
- * Abstract model class for objects which may have dynamic properties attached to them,
- * which is represented with an internal map.
+ * Abstract model class for a model which supports dynamic (arbitrary) properties of type T.
  */
-public abstract class DynamicModel extends HashMap<String, Object> implements ObjectModel {
+public abstract class DynamicModel<T> implements ObjectModel {
+  private TypeToken<T> additionalPropertyTypeToken;
+
+  // The set of dynamic properties associated with this object.
+  private Map<String, T> dynamicProperties = new HashMap<>();
+
+  /**
+   * Force use of 1-arg ctor.
+   */
+  @SuppressWarnings("unused")
+  private DynamicModel() {
+  }
+
+  /**
+   * This ctor accepts a TypeToken instance that represents the type of values stored in the map.
+   *
+   * @param t
+   *          the TypeToken which represents the type of map values
+   */
+  public DynamicModel(TypeToken<T> t) {
+    this.additionalPropertyTypeToken = t;
+  }
+
+  /**
+   * Returns the TypeToken which describes the type of additional properties stored in the map.
+   * @return The TypeToken which describes the map value type
+   */
+  public TypeToken<T> getAdditionalPropertyTypeToken() {
+    return this.additionalPropertyTypeToken;
+  }
+
+  /**
+   * Sets an arbitrary property.
+   *
+   * @param key
+   *          the name of the property to set
+   * @param value
+   *          the value of the property to be set
+   * @return the previous value of the property, or null if the property was not previously set
+   */
+  public T setProperty(String key, T value) {
+    return this.dynamicProperties.put(key, value);
+  }
+
+  /**
+   * Returns the value of the specified property.
+   *
+   * @param key
+   *          the name of the property to get
+   * @return the value of the property, or null if the property is not set
+   */
+  public T getProperty(String key) {
+    return this.dynamicProperties.get(key);
+  }
+
+  /**
+   * Returns a map containing the arbitrary properties set on this object.
+   *
+   * @return a copy of the map containing arbitrary properties set on this object
+   */
+  public Map<String, T> getProperties() {
+    return new HashMap<String, T>(this.dynamicProperties);
+  }
+
+  /**
+   * Returns the names of arbitrary properties set on this object.
+   * @return a set containing the names of arbitrary properties set on this object
+   */
+  public Set<String> getPropertyNames() {
+    return this.dynamicProperties.keySet();
+  }
+  /**
+   * Removes the specified property from this object's map of arbitrary properties.
+   *
+   * @param key
+   *          the name of the property to be removed
+   * @return the previous value of the property, or null if the property was not previously set
+   */
+  public T removeProperty(String key) {
+    return this.dynamicProperties.remove(key);
+  }
+
+  /**
+   * Removes all of the arbitrary properties set on this object.
+   */
+  public void removeProperties() {
+    this.dynamicProperties.clear();
+  }
 
   /*
    * (non-Javadoc)
@@ -23,7 +112,7 @@ public abstract class DynamicModel extends HashMap<String, Object> implements Ob
       return false;
     }
 
-    final DynamicModel other = (DynamicModel) o;
+    final DynamicModel<?> other = (DynamicModel<?>) o;
 
     return toString().equals(other.toString());
   }
---
+++ src/test/java/com/ibm/cloud/sdk/core/test/model/generated/ModelAPFooBad.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2018 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.ibm.cloud.sdk.core.test.model.generated;
+
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
+import com.ibm.cloud.sdk.core.service.model.DynamicModel;
+
+/**
+ * Model with additionalProperties set to ref to named schema.
+ */
+public class ModelAPFooBad extends DynamicModel<Foo> {
+  @SerializedName(value="prop1", alternate={"property1", "p1"})
+  private String prop1;
+  @SerializedName(value="prop2", alternate={"property2", "p1"})   // <<---- Intentionally incorrect.
+  private Long prop2;
+
+  public ModelAPFooBad() {
+    super(new TypeToken<Foo>(){});
+  }
+
+  /**
+   * Gets the prop1.
+   *
+   * String property.
+   *
+   * @return the prop1
+   */
+  public String prop1() {
+    return this.prop1;
+  }
+
+  /**
+   * Sets the prop1.
+   *
+   * @param prop1 the new prop1
+   */
+  public void setProp1(final String prop1) {
+    this.prop1 = prop1;
+  }
+
+  /**
+   * Gets the prop2.
+   *
+   * Integer property.
+   *
+   * @return the prop2
+   */
+  public Long prop2() {
+    return this.prop2;
+  }
+
+  /**
+   * Sets the prop2.
+   *
+   * @param prop2 the new prop2
+   */
+  public void setProp2(final long prop2) {
+    this.prop2 = prop2;
+  }
+}
