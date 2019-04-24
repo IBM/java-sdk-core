992ea6cdadf6bfd990fe7890b26f819bccb4e909
Logan Patino <loganpatino10@gmail.com>
2019-04-24 09:43:09 -0400

    ci(docs): Add release docs to GH pages site on releases

---
+++ build/push-javadoc-to-gh-pages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" ]; then
+
+  git config --global user.email "wps@us.ibm.com"
+  git config --global user.name "Watson Github Bot"
+  git clone --quiet --branch=gh-pages https://${GITHUB_OAUTH_TOKEN}@github.com/IBM/java-sdk-core.git gh-pages > /dev/null
+
+  pushd gh-pages
+    # on tagged builds, $TRAVIS_BRANCH is the tag (e.g. v1.2.3), otherwise it's the branch name (e.g. master)
+    rm -rf docs/$TRAVIS_BRANCH
+    mkdir -p docs/$TRAVIS_BRANCH
+    cp -rf ../target/apidocs/* docs/$TRAVIS_BRANCH
+    ../.utility/generate_index_html.sh > index.html
+
+    git add -f .
+    git commit -m "Latest javadoc for $TRAVIS_BRANCH ($TRAVIS_COMMIT)"
+    git push -fq origin gh-pages > /dev/null
+
+  popd
+
+  echo -e "Published Javadoc for build $TRAVIS_BUILD_NUMBER ($TRAVIS_JOB_NUMBER) on branch $TRAVIS_BRANCH.\n"
+
+else
+
+  echo -e "Not publishing docs for build $TRAVIS_BUILD_NUMBER ($TRAVIS_JOB_NUMBER) on branch $TRAVIS_BRANCH of repo $TRAVIS_REPO_SLUG"
+
+fi
