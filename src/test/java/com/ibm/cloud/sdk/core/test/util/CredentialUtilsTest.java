9f2e4f5ceba5da00c5b5f5411896780680fdd7a4
Logan Patino <loganpatino10@gmail.com>
2019-07-08 13:48:36 -0400

    test: Add test for loading ICP4D values from file

--- src/test/java/com/ibm/cloud/sdk/core/test/util/CredentialUtilsTest.java
+++ src/test/java/com/ibm/cloud/sdk/core/test/util/CredentialUtilsTest.java
@@ -42,6 +42,11 @@ public class CredentialUtilsTest {
   private static final String NLC_SERVICE_NAME = "natural_language_classifier";
   private static final String TEST_APIKEY = "123456789";
   private static final String NLC_URL = "https://gateway.watsonplatform.net/natural-language-classifier/api";
+  private static final String ASSISTANT_SERVICE_NAME = "assistant";
+  private static final String TEST_ACCESS_TOKEN =
+      "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6IkFkbWluIiwicGVybWlzc2lvbnMiOlsiYWRtaW5pc3RyYXRvciIsIm1hbmFnZV9jYXRhbG9nIiwiYWNjZXNzX2NhdGFsb2ciLCJtYW5hZ2VfcG9saWNpZXMiLCJhY2Nlc3NfcG9saWNpZXMiLCJ2aXJ0dWFsaXplX3RyYW5zZm9ybSIsImNhbl9wcm92aXNpb24iLCJkZXBsb3ltZW50X2FkbWluIl0sInN1YiI6ImFkbWluIiwiaXNzIjoiS05PWFNTTyIsImF1ZCI6IkRTWCIsInVpZCI6Ijk5OSIsImlhdCI6MTU1OTMyODk1NSwiZXhwIjo5OTk5OTk5OTk5OTk5OTk5OTl9.GE-ML3JWmI3oB0z5mjMG3jFtYVVA5bQCsOTOOj9ab7PcgJc1mA5hn1sONkO0JAFADhUoAgpG4KgQef5tjnCSrtl1tbnDuhaP1DH4QKMCZOkWrKyfQ2X8P1jhyJmV-KpE4wuTrGdMoMVj4wVRZwnxMRSK6LhV6pIzyOLLYR21zcW_2KcKWxCYfIC7tiM1d2PSM5nWa_5Vb068F8PtdiFUElEYHYKrvmwpV57_k2jpXoY6zw8PDcIiWQe3g20w6vCB6zWhxbcFWyjMg1tPOZHgTNNskPShHQBbtZFsSrc7rkNPzttKF70m7_JqrRYUZDNN8TmuR9uyitwxEFkr2L0WDQ";
+  private static final String TEST_ICP_INSTANCE_URL = "https://icp" +
+      ".cluster:31843/assistant/test/instances/1560453115/api";
 
   /**
    * Setup.
@@ -123,5 +128,14 @@ public class CredentialUtilsTest {
     assertNull(emptyCredentials.getUrl());
     assertNull(emptyCredentials.getIamApiKey());
     assertNull(emptyCredentials.getIamUrl());
+  }
+
+  @Test
+  public void testGetFileCredentialsIcp4d() {
+    CredentialUtils.ServiceCredentials testCredentials
+        = CredentialUtils.getFileCredentials(ASSISTANT_SERVICE_NAME);
+
+    assertEquals(TEST_ACCESS_TOKEN, testCredentials.getAccessToken());
+    assertEquals(TEST_ICP_INSTANCE_URL, testCredentials.getUrl());
   }
 }
