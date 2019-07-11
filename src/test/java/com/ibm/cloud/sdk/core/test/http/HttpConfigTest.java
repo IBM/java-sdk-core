4345c0cd50e3890f22b6ec788f91a084697d2b5b
Marlon Carvalho <marlon.carvalho@gmail.com>
2019-07-11 13:15:13 -0300

    Allow for authentication for configured proxy

--- src/test/java/com/ibm/cloud/sdk/core/test/http/HttpConfigTest.java
+++ src/test/java/com/ibm/cloud/sdk/core/test/http/HttpConfigTest.java
@@ -1,9 +1,15 @@
 package com.ibm.cloud.sdk.core.test.http;
 
+import okhttp3.Authenticator;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.Route;
 import org.junit.Test;
 
 import com.ibm.cloud.sdk.core.http.HttpConfigOptions;
 
+import javax.annotation.Nullable;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 
@@ -16,13 +22,23 @@ public class HttpConfigTest {
 
   @Test
   public void testHttpConfigOptions() {
+    Authenticator authenticator = new Authenticator() {
+      @Nullable
+      @Override
+      public Request authenticate(@Nullable Route route, Response response) throws IOException {
+        return null;
+      }
+    };
+
     Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxyHost", 8080));
     HttpConfigOptions configOptions = new HttpConfigOptions.Builder()
         .disableSslVerification(true)
         .proxy(proxy)
+            .proxyAuthenticator(authenticator)
         .build();
 
     assertEquals(true, configOptions.shouldDisableSslVerification());
+    assertEquals(authenticator, configOptions.getProxyAuthenticator());
     assertEquals(proxy, configOptions.getProxy());
   }
 }
