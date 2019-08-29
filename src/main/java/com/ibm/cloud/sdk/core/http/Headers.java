d29297fdb8a2e9e399018109e9887c799ab784be
Phil Adams <phil_adams@us.ibm.com>
2019-08-29 09:48:39 -0500

    fix: updated copyright statements

--- src/main/java/com/ibm/cloud/sdk/core/http/Headers.java
+++ src/main/java/com/ibm/cloud/sdk/core/http/Headers.java
@@ -1,3 +1,16 @@
+/**
+ * (C) Copyright IBM Corp. 2015, 2019.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package com.ibm.cloud.sdk.core.http;
 
 import java.util.List;
@@ -20,14 +33,17 @@ public class Headers {
    * @param other the other object to compare
    * @return whether the two objects are equal or not
    */
+  @Override
   public boolean equals(Object other) {
     return this.headers.equals(other);
   }
 
+  @Override
   public int hashCode() {
     return this.headers.hashCode();
   }
 
+  @Override
   public String toString() {
     return this.headers.toString();
   }
