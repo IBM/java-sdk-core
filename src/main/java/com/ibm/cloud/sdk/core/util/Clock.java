04eb4ed3f9f32df5757bfc6ff2157f2fd0044b72
Logan Patino <loganpatino10@gmail.com>
2019-11-22 14:28:43 -0500

    refactor: Add wrapper class for getting current time to allow for easier testing

---
+++ src/main/java/com/ibm/cloud/sdk/core/util/Clock.java
@@ -0,0 +1,15 @@
+package com.ibm.cloud.sdk.core.util;
+
+/**
+ * Wrapper class for getting the current time. This class is especially useful for mocking the current time in tests.
+ */
+public class Clock {
+
+  public static long getCurrentTimeInMillis() {
+    return System.currentTimeMillis();
+  }
+
+  public static long getCurrentTimeInSeconds() {
+    return System.currentTimeMillis() / 1000;
+  }
+}
