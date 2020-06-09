e34997962a7da54d3554a37bce336db221a57fb4
Phil Adams <phil_adams@us.ibm.com>
2020-06-09 12:29:28 -0500

    feat: RequestBuilder changes to support JSON streaming feature
    Fixes arf/planning-sdk-squad#901
    
    The RequestBuilder.bodyContent() method is modified to no
    longer use the contentType parameter to determine which one of the
    other three input parameters should be used to create the request body.
    Instead, we now use the first non-null value using the order
    'jsonContent', 'jsonPatchContent', then 'nonJsonContent'.
    
    This change is necessary for us to implement the JSON streaming
    feature, where the SDK user might serialize the JSON content
    himself and pass it in as a String or InputStream while also
    using a contentType that would otherwise indicate a JSON body.

--- src/test/java/com/ibm/cloud/sdk/core/test/service/RequestBuilderTest.java
+++ src/test/java/com/ibm/cloud/sdk/core/test/service/RequestBuilderTest.java
@@ -293,7 +293,7 @@ public class RequestBuilderTest {
     assertNotNull(url);
     assertEquals("https://myserver.com/testservice/api/discovery", url.toString());
   }
-  
+
   @Test
   public void testConstructHttpUrlEmptyPath2() {
     String[] pathSegments = { "" };
@@ -333,13 +333,66 @@ public class RequestBuilderTest {
   public void testConstructHttpUrlEmpty() {
     String[] pathSegments = { "v1/seg1", "seg2", "seg3"};
     String[] pathParameters = { "param1", "param2" };
-    HttpUrl url = RequestBuilder.constructHttpUrl("", pathSegments, pathParameters);
+    RequestBuilder.constructHttpUrl("", pathSegments, pathParameters);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testConstructHttpUrlNull() {
     String[] pathSegments = { "v1/seg1", "seg2", "seg3"};
     String[] pathParameters = { "param1", "param2" };
-    HttpUrl url = RequestBuilder.constructHttpUrl(null, pathSegments, pathParameters);
+    RequestBuilder.constructHttpUrl(null, pathSegments, pathParameters);
   }
+
+  /**
+   * Test bodyContent() with a model instance.
+   * @throws IOException
+   */
+  @Test
+  public void testBodyContent1() throws IOException {
+    Truck truck = new Truck.Builder().vehicleType("Truck").make("Ford").engineType("raptor").build();
+    final Request request = RequestBuilder.post(HttpUrl.parse(urlWithQuery))
+        .bodyContent("application/json", truck, null, (InputStream) null).build();
+    final RequestBody requestedBody = request.body();
+    final Buffer buffer = new Buffer();
+    requestedBody.writeTo(buffer);
+
+    assertEquals(GsonSingleton.getGsonWithoutPrettyPrinting().toJson(truck), buffer.readUtf8());
+    assertEquals(HttpMediaType.JSON, requestedBody.contentType());
+  }
+
+  /**
+   * Test bodyContent() with an already serialized model instance (a String).
+   * @throws IOException
+   */
+  @Test
+  public void testBodyContent2() throws IOException {
+    Truck truck = new Truck.Builder().vehicleType("Truck").make("Ford").engineType("raptor").build();
+    String jsonString = GsonSingleton.getGsonWithoutPrettyPrinting().toJson(truck);
+    final Request request = RequestBuilder.post(HttpUrl.parse(urlWithQuery))
+        .bodyContent("application/json", null, null, jsonString).build();
+    final RequestBody requestedBody = request.body();
+    final Buffer buffer = new Buffer();
+    requestedBody.writeTo(buffer);
+
+    assertEquals(jsonString, buffer.readUtf8());
+    assertEquals(MediaType.parse("application/json"), requestedBody.contentType());
+  }
+
+  /**
+   * Test bodyContent() with a multiple inputs (JSON input should win).
+   * @throws IOException
+   */
+  @Test
+  public void testBodyContent3() throws IOException {
+    Truck truck = new Truck.Builder().vehicleType("Truck").make("Ford").engineType("raptor").build();
+    final Request request = RequestBuilder.post(HttpUrl.parse(urlWithQuery))
+        .bodyContent("application/json", truck, "BAD JSON PATCH BODY", "BAD INPUTSTREAM REQUEST BODY").build();
+    final RequestBody requestedBody = request.body();
+    final Buffer buffer = new Buffer();
+    requestedBody.writeTo(buffer);
+
+    assertEquals(GsonSingleton.getGsonWithoutPrettyPrinting().toJson(truck), buffer.readUtf8());
+    assertEquals(HttpMediaType.JSON, requestedBody.contentType());
+  }
+
 }
