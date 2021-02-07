ecadef407c6f1f8f435c05ef4cb93fcba64afb22
Phil Adams <phil_adams@us.ibm.com>
2021-02-07 10:48:55 -0600

    fix(build): publish artifacts directly to maven central repository
    This commit modifies the build configuration so that build artifacts
    are published to the Maven Central repository (via the ossrh
    staging repo).  Previously, build artifacts were published to
    the bintray/jcenter repository, then mirrored to Maven Central.
    But, with the imminent demise of bintray/jcenter, we need to
    publish directly to Maven Central.  Going forward, published jars
    will be available only on Maven Central.

---
+++ build/generateJavadocIndex.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# based on https://odoepner.wordpress.com/2012/02/17/shell-script-to-generate-simple-index-html/
+
+echo '<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>IBM Java SDK Core</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="page-header">
+        <h1>IBM Cloud Java SDK Core Library Documentation</h1>
+    </div>
+    <p>Javadoc by branch/release:</p>
+    <ul>
+        <li><a href="docs/latest">Latest</a></li>'
+ls docs | grep --invert-match index.html | sed 's/^.*/        <li><a href="docs\/&">&<\/a><\/li>/'
+echo '    </ul>
+</div>
+</body>
+</html>'
---
+++ build/publishJavadoc.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Publish javadocs only for a tagged-release or non-PR build of master.
+if [[ -n "${TRAVIS_TAG}" || "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+
+    printf "\n>>>>> Publishing javadoc for release build: repo=%s branch=%s build_num=%s job_num=%s\n" ${TRAVIS_REPO_SLUG} ${TRAVIS_BRANCH} ${TRAVIS_BUILD_NUMBER} ${TRAVIS_JOB_NUMBER} 
+
+    printf "\n>>>>> Cloning repository's gh-pages branch into directory 'gh-pages'\n"
+    rm -fr ./gh-pages
+    git clone --branch=gh-pages https://${GH_TOKEN}@github.com/IBM/java-sdk-core.git gh-pages
+
+    printf "\n>>>>> Finished cloning...\n"
+
+    pushd gh-pages
+    
+    # Create a new directory for this branch/tag and copy the javadocs there.
+    printf "\n>>>>> Copying javadocs to new directory: docs/%s\n" ${TRAVIS_BRANCH}
+    rm -rf docs/${TRAVIS_BRANCH}
+    mkdir -p docs/${TRAVIS_BRANCH}
+    cp -rf ../target/site/apidocs/* docs/${TRAVIS_BRANCH}
+
+    printf "\n>>>>> Generating gh-pages index.html...\n"
+    ../build/generateJavadocIndex.sh > index.html
+
+    # Update the 'latest' symlink to point to this branch if it's a tagged release.
+    if [ -n "$TRAVIS_TAG" ]; then
+	pushd docs
+	rm latest
+	ln -s ./${TRAVIS_TAG} latest
+	printf "\n>>>>> Updated 'docs/latest' symlink:\n"
+	ls -l latest
+	popd
+    fi
+
+    printf "\n>>>>> Committing new javadoc...\n"
+    git add -f .
+    git commit -m "doc: latest javadoc for ${TRAVIS_BRANCH} (${TRAVIS_COMMIT})"
+    git push -f origin gh-pages
+
+    popd
+
+    printf "\n>>>>> Published javadoc for release build: repo=%s branch=%s build_num=%s job_num=%s\n"  ${TRAVIS_REPO_SLUG} ${TRAVIS_BRANCH} ${TRAVIS_BUILD_NUMBER} ${TRAVIS_JOB_NUMBER} 
+
+else
+
+    printf "\n>>>>> Javadoc publishing bypassed for non-release build: repo=%s branch=%s build_num=%s job_num=%s\n" ${TRAVIS_REPO_SLUG} ${TRAVIS_BRANCH} ${TRAVIS_BUILD_NUMBER} ${TRAVIS_JOB_NUMBER} 
+
+fi
+
