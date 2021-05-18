81cd4a67c84a268e8a5a2df05655fefe705038ec
Rich Ellis <ricellis@users.noreply.github.com>
2021-05-18 11:12:00 +0100

    fix(http): allow tlsv1.3 and disable tlsv1.0 and v1.1

---
+++ src/test/java/com/ibm/cloud/sdk/core/test/http/HttpClientSingletonTest.java
@@ -0,0 +1,68 @@
+/**
+ * (C) Copyright IBM Corp. 2021.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.ibm.cloud.sdk.core.test.http;
+
+import com.ibm.cloud.sdk.core.http.HttpClientSingleton;
+import com.ibm.cloud.sdk.core.http.HttpConfigOptions;
+import okhttp3.ConnectionSpec;
+import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit tests for the HttpClientSingleton object.
+ */
+public class HttpClientSingletonTest {
+
+    private void getAndAssertEnabledProtocols(OkHttpClient client) throws IOException {
+        SSLSocket sslSocket = (SSLSocket) client.sslSocketFactory().createSocket();
+        List<String> enabledProtocols = Arrays.asList(sslSocket.getEnabledProtocols());
+        // Assert there is at least 1 enabled protocol
+        assertTrue("There should be at least 1 TLS protocol enabled.", enabledProtocols.size() > 0);
+
+        // Get the MODERN_TLS Java names and the runtime supported protocols
+        List<String> modernTlsNames = new ArrayList<>();
+        for (TlsVersion tlsVersion : ConnectionSpec.MODERN_TLS.tlsVersions()) {
+            modernTlsNames.add(tlsVersion.javaName());
+        }
+        List<String> supportedProtocols = Arrays.asList(sslSocket.getSupportedProtocols());
+        // Iterate and assert that each enabled protocol is present in both lists
+        for (String protocol : enabledProtocols) {
+            // Assert that the enabled protocols is supported by the runtime
+            assertTrue(String.format("The enabled protocol %s should be supported by the runtime", protocol),
+                    supportedProtocols.contains(protocol));
+            // Assert that the enabled protocols is present in MODERN_TLS
+            assertTrue(String.format("The enabled protocol %s should be in the MODERN_TLS connection spec", protocol),
+                    modernTlsNames.contains(protocol));
+        }
+    }
+
+    @Test
+    public void testTlsProtocolFiltering() throws IOException {
+        OkHttpClient client = HttpClientSingleton.getInstance().createHttpClient();
+        getAndAssertEnabledProtocols(client);
+    }
+
+}
