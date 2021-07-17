4f3ea34cdbb4d3a84bb450b0435fe7da26e32c7b
András Csányi <andras.csanyi@pm.me>
2021-07-17 20:11:28 +0200

    test: cover LongToDateTimeAdapter by unit tests

--- src/test/java/com/ibm/cloud/sdk/core/util/LongToDateTimeAdapterTest.java
+++ src/test/java/com/ibm/cloud/sdk/core/util/LongToDateTimeAdapterTest.java
@@ -9,25 +9,129 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
 
 public class LongToDateTimeAdapterTest {
   private Gson gson;
+  private String name = "Lorem";
 
   @Test
-  public void writeShouldSerializeDateNullToJsonNullValue() {
+  public void writeShouldSerializeDateNullValueToJsonNullValue() {
     // Arrange
-    LongToDateTimeAdatpterModel model = new LongToDateTimeAdatpterModel();
+    LongToDateTimeAdapterModel model = new LongToDateTimeAdapterModel();
     model.birthDate = null;
-    model.name = "Lorem";
+    model.name = name;
 
     // Act
     String serialized = gson.toJson(model);
 
     // Assert
-    assertTrue(serialized.contains("\"name\":\"Lorem\""));
+    assertTrue(serialized.contains(String.format("\"name\":\"%s\"", name)));
     assertFalse(serialized.contains("birth_date"));
   }
+
+  @Test
+  public void writeShouldSerializeDateNowValueToJsonValue() {
+    // Arrange
+    Date date = new Date();
+    LongToDateTimeAdapterModel model = new LongToDateTimeAdapterModel();
+    model.birthDate = date;
+    model.name = name;
+
+    // Act
+    String serialized = gson.toJson(model);
+
+    // Assert
+    assertTrue(serialized.contains(String.format("\"name\":\"%s\"", name)));
+    assertTrue(serialized.contains(String.format("\"birth_date\":%d", date.getTime())));
+  }
+
+  @Test
+  public void writeShouldSerializeDateWithPriorUnixTimeStartValueToJsonValue() {
+    // Arrange
+    LongToDateTimeAdapterModel model = new LongToDateTimeAdapterModel();
+    model.birthDate = new Date(-31489139000L);
+    model.name = name;
+
+    // Act
+    String serialized = gson.toJson(model);
+
+    // Assert
+    assertTrue(serialized.contains(String.format("\"name\":\"%s\"", name)));
+    assertTrue(serialized.contains(String.format("\"birth_date\":%d", model.birthDate.getTime())));
+  }
+
+  @Test
+  public void readShouldDeserializeJsonNullValueToDateNull() {
+    // Arrange
+    String json = String.format("{\"birth_date\":null, \"name\":\"%s\"}", name);
+
+    // Act
+    LongToDateTimeAdapterModel model = gson.fromJson(json, LongToDateTimeAdapterModel.class);
+
+    // Assert
+    assertNull(model.birthDate);
+    assertEquals(model.name, name);
+  }
+
+  @Test(expectedExceptions = NumberFormatException.class)
+  public void readShouldThrowWhenDateJsonValueIsEmptyString() {
+    // Arrange
+    String json = String.format("{\"birth_date\":\"\", \"name\":\"%s\"}", name);
+
+    // Act
+    LongToDateTimeAdapterModel model = gson.fromJson(json, LongToDateTimeAdapterModel.class);
+
+    // Assert
+    assertNull(model.birthDate);
+    assertEquals(model.name, "Lorem");
+  }
+
+  @Test(expectedExceptions = NumberFormatException.class)
+  public void readShouldThrowWhenJsonDateValueIsSpaceString() {
+    // Arrange
+    String json = String.format("{\"birth_date\":\" \", \"name\":\"%s\"}", name);
+
+    // Act
+    LongToDateTimeAdapterModel model = gson.fromJson(json, LongToDateTimeAdapterModel.class);
+
+    // Assert
+    assertNull(model.birthDate);
+    assertEquals(model.name, "Lorem");
+  }
+
+  @Test
+  public void readShouldDeserializeWhenJsonValueIsNumber() {
+    // Arrange
+    Date date = new Date();
+    String json = String.format("{\"birth_date\":%d, \"name\":\"%s\"}", date.getTime(), name);
+
+    // Act
+    LongToDateTimeAdapterModel model = gson.fromJson(json, LongToDateTimeAdapterModel.class);
+
+    // Assert
+    assertNotNull(model.birthDate);
+    assertEquals(model.birthDate.getTime(), date.getTime());
+    assertEquals(model.name, "Lorem");
+  }
+
+  @Test
+  public void readShouldDeserializeWhenJsonValueIsStringButNumberInIt() {
+    // Arrange
+    Date date = new Date();
+    String json = String.format("{\"birth_date\":\"%d\", \"name\":\"%s\"}", date.getTime(), name);
+
+    // Act
+    LongToDateTimeAdapterModel model = gson.fromJson(json, LongToDateTimeAdapterModel.class);
+
+    // Assert
+    assertNotNull(model.birthDate);
+    assertEquals(model.birthDate.getTime(), date.getTime());
+    assertEquals(model.name, "Lorem");
+  }
+
 
   @BeforeMethod
   public void beforeMethod() {
@@ -36,7 +140,7 @@ public class LongToDateTimeAdapterTest {
     gson = gsonBuilder.create();
   }
 
-  private class LongToDateTimeAdatpterModel {
+  private class LongToDateTimeAdapterModel {
     @SerializedName("birth_date")
     public Date birthDate;
 
