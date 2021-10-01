d8e1931799a74267447ba5fc5ececd697d665f8a
Phil Adams <padamstx@gmail.com>
2021-09-30 19:24:17 -0500

    chore(build): fix gpg import (#148)

--- build/setupSigning.sh
+++ build/setupSigning.sh
@@ -13,7 +13,7 @@ echo "Importing signing key..."
 openssl aes-256-cbc -K $encrypted_4b7d603e7466_key -iv $encrypted_4b7d603e7466_iv -in build/signing.key.enc -out build/signing.key -d
 
 gpg --version
-gpg --fast-import build/signing.key
+gpg --batch --import build/signing.key
 rm build/signing.key
 
 echo "Signing key import finished!"
