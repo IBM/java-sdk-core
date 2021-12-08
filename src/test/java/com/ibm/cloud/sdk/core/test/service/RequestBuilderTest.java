fc26f41c31232487a20df280185b766414c1ef45
HildoYe <hildomy5@gmail.com>
2021-12-08 13:21:19 -0600

    Ignore JSON ordering difference

--- src/test/java/com/ibm/cloud/sdk/core/test/service/RequestBuilderTest.java
+++ src/test/java/com/ibm/cloud/sdk/core/test/service/RequestBuilderTest.java
@@ -25,6 +25,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.testng.annotations.Test;
 
 import com.google.gson.JsonObject;
@@ -211,9 +213,10 @@ public class RequestBuilderTest {
    * Test with list of models.
    *
    * @throws IOException Signals that an I/O exception has occurred.
+   * @throws JSONException Signals that a JSON parsing error has occured.
    */
   @Test
-  public void testBodyContentList() throws IOException {
+  public void testBodyContentList() throws IOException, JSONException {
     // add list of actual models
     final List<Vehicle> listOfModels = new ArrayList<>();
     listOfModels.add(new Truck.Builder().vehicleType("Truck").make("Ford").engineType("raptor").build());
@@ -224,8 +227,11 @@ public class RequestBuilderTest {
     final RequestBody requestedBody = request.body();
     final Buffer buffer = new Buffer();
     requestedBody.writeTo(buffer);
-
-    assertEquals(GsonSingleton.getGsonWithoutPrettyPrinting().toJson(listOfModels), buffer.readUtf8());
+    try {
+      JSONAssert.assertEquals(GsonSingleton.getGsonWithoutPrettyPrinting().toJson(listOfModels), buffer.readUtf8(), false);
+    } catch (JSONException e) {
+      throw e;
+    }
     assertEquals(HttpMediaType.JSON, requestedBody.contentType());
   }
 
@@ -475,17 +481,21 @@ public class RequestBuilderTest {
   /**
    * Test bodyContent() with a model instance.
    * @throws IOException
+   * @throws JSONException
    */
   @Test
-  public void testBodyContent1() throws IOException {
+  public void testBodyContent1() throws IOException, JSONException {
     Truck truck = new Truck.Builder().vehicleType("Truck").make("Ford").engineType("raptor").build();
     final Request request = RequestBuilder.post(HttpUrl.parse(urlWithQuery))
         .bodyContent("application/json", truck, null, (InputStream) null).build();
     final RequestBody requestedBody = request.body();
     final Buffer buffer = new Buffer();
     requestedBody.writeTo(buffer);
-
-    assertEquals(GsonSingleton.getGsonWithoutPrettyPrinting().toJson(truck), buffer.readUtf8());
+    try {
+      JSONAssert.assertEquals(GsonSingleton.getGsonWithoutPrettyPrinting().toJson(truck), buffer.readUtf8(), false);
+    } catch (JSONException e) {
+      throw e;
+    }
     assertEquals(HttpMediaType.JSON, requestedBody.contentType());
   }
 
@@ -502,7 +512,6 @@ public class RequestBuilderTest {
     final RequestBody requestedBody = request.body();
     final Buffer buffer = new Buffer();
     requestedBody.writeTo(buffer);
-
     assertEquals(jsonString, buffer.readUtf8());
     assertEquals(MediaType.parse("application/json"), requestedBody.contentType());
   }
@@ -510,9 +519,10 @@ public class RequestBuilderTest {
   /**
    * Test bodyContent() with a multiple inputs (JSON input should win).
    * @throws IOException
+   * @throws JSONException
    */
   @Test
-  public void testBodyContent3() throws IOException {
+  public void testBodyContent3() throws IOException, JSONException {
     Truck truck = new Truck.Builder().vehicleType("Truck").make("Ford").engineType("raptor").build();
     final Request request = RequestBuilder.post(HttpUrl.parse(urlWithQuery))
         .bodyContent("application/json", truck, "BAD JSON PATCH BODY", "BAD INPUTSTREAM REQUEST BODY").build();
@@ -520,7 +530,11 @@ public class RequestBuilderTest {
     final Buffer buffer = new Buffer();
     requestedBody.writeTo(buffer);
 
-    assertEquals(GsonSingleton.getGsonWithoutPrettyPrinting().toJson(truck), buffer.readUtf8());
+    try {
+      JSONAssert.assertEquals(GsonSingleton.getGsonWithoutPrettyPrinting().toJson(truck), buffer.readUtf8(), false);
+    } catch (JSONException e) {
+      throw e;
+    }
     assertEquals(HttpMediaType.JSON, requestedBody.contentType());
   }
 
