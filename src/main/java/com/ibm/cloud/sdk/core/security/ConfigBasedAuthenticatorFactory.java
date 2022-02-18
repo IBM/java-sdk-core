dad41dced3f55c9372d47739e31b962b2bc896b2
Phil Adams <phil_adams@us.ibm.com>
2022-02-18 12:43:37 -0600

    fix: set visibility of ConfigBasedAuthenticatorFactory.createAuthenticator to protected
    This commit changes the visibility of the
    "ConfigBasedAuthenticatorFactory.createAuthenticator()" method from
    private to protected to allow SDKs to override this class
    in order to support alternate configuration sources
    (e.g. injected spring properties).

--- src/main/java/com/ibm/cloud/sdk/core/security/ConfigBasedAuthenticatorFactory.java
+++ src/main/java/com/ibm/cloud/sdk/core/security/ConfigBasedAuthenticatorFactory.java
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright IBM Corp. 2019, 2021.
+ * (C) Copyright IBM Corp. 2019, 2022.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -58,7 +58,7 @@ public class ConfigBasedAuthenticatorFactory {
    * @param props a Map containing configuration properties
    * @return an Authenticator instance
    */
-  private static Authenticator createAuthenticator(Map<String, String> props) {
+  protected static Authenticator createAuthenticator(Map<String, String> props) {
     Authenticator authenticator = null;
 
     // If auth type was not specified, we'll use "iam" as the default if the "apikey" property
