84af366208827b6ccde9ede9704b106113a25fdc
Phil Adams <phil_adams@us.ibm.com>
2022-03-16 15:05:50 -0500

    fix: bump gson to 2.9.0 (#170)

--- src/test/java/com/ibm/cloud/sdk/core/test/model/DynamicModelTest.java
+++ src/test/java/com/ibm/cloud/sdk/core/test/model/DynamicModelTest.java
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright IBM Corp. 2015, 2019.
+ * (C) Copyright IBM Corp. 2015, 2022.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -23,7 +23,7 @@ import org.testng.annotations.Test;
 import com.ibm.cloud.sdk.core.test.model.generated.AnimalCat;
 
 /**
- * A few simple tests that exercise the GenericModel methods.
+ * A few simple tests that exercise the DynamicModel methods.
  */
 public class DynamicModelTest {
   private boolean displayOutput = false;
