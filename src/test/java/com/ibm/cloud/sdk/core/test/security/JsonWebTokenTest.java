a3fabebf8c013ee7b216b44addb8b4c1b73df157
Norbert Biczo <pyrooka@users.noreply.github.com>
2026-09-22 16:32:55 +0200

    chore: add disclaimer about test tokens
    Signed-off-by: Norbert Biczo <pyrooka@users.noreply.github.com>

--- src/test/java/com/ibm/cloud/sdk/core/test/security/JsonWebTokenTest.java
+++ src/test/java/com/ibm/cloud/sdk/core/test/security/JsonWebTokenTest.java
@@ -10,6 +10,9 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
+
+// DISCLAIMER: The JWT token strings used in this file are for testing purposes only.
+// They are not valid tokens and cannot be used to authenticate with any real service.
 package com.ibm.cloud.sdk.core.test.security;
 
 import org.testng.annotations.Test;
